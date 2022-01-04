11e3902b5873c3654c4bb7d2a04d17c5af719c05
myAdmin <voydan93@gmail.com>
2022-01-04 11:54:53 +0200

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,74 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
 
 #define ONE_WIRE_BUS D2
 #define ADC_A0 A0
 
-float volt = 0.0;
-float vin = 0.0;  
+#define BOT_TOKEN "50281"
+#define CHAT_ID "30"
+
+const char* ssid = "ASUS_GUEST";
+const char* password = "QaS33687";
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
 float r1 = 98820;
 float r2 = 9937;
 
-int timeWake = 0;
-
 int value = 0;
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+uint8 wifiIncriment = 0;
 
-// Pass our oneWire reference to Dallas Temperature. 
+
+OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire);
 
-// arrays to hold device addresses
 
+ String textMessage(){
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  String ytempC = String(sensors.getTempCByIndex(0));
+  String btempC = String(sensors.getTempCByIndex(1));
+
+  value = analogRead(ADC_A0);
+  float volt = (value * 3.2)/1023.0;
+  float vin = volt / (r2 / (r1+r2));
+  String batt = String(vin);
+  delay(500);
+
+  return "Yard " + ytempC + "C \n" + "Balcony " + btempC + "C \n" + "VCC " + batt + "v";
+ }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(ADC_A0,INPUT);
   sensors.begin();
+
+  WiFi.begin(ssid,password); 
+  secured_client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      wifiIncriment++;
+      if (wifiIncriment == 10){
+      ESP.deepSleep(400e7);
+      }
+    }
   
-  
+  configTime(0, 0, "pool.ntp.org");
+
+  bot.sendMessage(CHAT_ID, textMessage(), "");
+  ESP.deepSleep(400e7);
 }
 
-void countsleep(){
-  timeWake++;
-  Serial.println(timeWake);
-  ESP.deepSleep(30e6);
-  
-}
 
 void loop() {
-
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  float atempC = sensors.getTempCByIndex(0);
-  float btempC = sensors.getTempCByIndex(1);
-  Serial.print("DS18B20: ");
-  Serial.print(atempC);
-  Serial.println("C");
-
-  value = analogRead(ADC_A0);
-  volt = (value * 3.0)/1023.0;
-  vin = volt / (r2 / (r1+r2));
-
-  Serial.print("V - ");
-  Serial.println(vin);
-  delay(1000);
-  
-  //countsleep();
-  
-}
+ 
+ }
